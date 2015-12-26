3a64a83239a8caff76ab48356ad37a9ab78a2271
Wang Boyu <boyu.wby@gmail.com>
2015-12-26 21:05:05 +0800

    add wikipedia reference

--- 002-add-digits/boyus.java
+++ 002-add-digits/boyus.java
@@ -3,6 +3,11 @@
  * To differ from others' solution files, rename to yourusername.java
  */
 
+/**
+ * For more information about this solution,
+ * refer to https://en.wikipedia.org/wiki/Digital_root
+ */
+
 public class Solution {
   public int addDigits(int num) {
     return 1 + (num - 1) % 9;
