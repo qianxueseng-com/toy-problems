bf50316e84634331478da00334a56c808259a8f7
Wang Boyu <boyu.wby@gmail.com>
2015-12-26 21:00:48 +0800

    boyu's solution in Java

---
+++ 002-add-digits/boyus.java
@@ -0,0 +1,10 @@
+/**
+ * File name should be Solution.java
+ * To differ from others' solution files, rename to yourusername.java
+ */
+
+public class Solution {
+  public int addDigits(int num) {
+    return 1 + (num - 1) % 9;
+  }
+}
