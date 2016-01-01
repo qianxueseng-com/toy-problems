45cbbaf98ed4c60b94793df2e7ff5c138ea31802
byenow <byenow1995@gmail.com>
2016-01-01 15:34:30 +0800

    Update byenow_003.js

--- 003-single-number/byenow_003.js
+++ 003-single-number/byenow_003.js
@@ -2,6 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
+ /**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ //排序，比较相邻项
+var singleNumber = function(nums) {
+  nums.sort(function sortNums(a,b){
+      return a-b;
+  });
+  for(var i=0,len=nums.length;i<len;i+=2){
+      if(nums[i]!==nums[i+1]){
+          return nums[i];
+      }
+  }
+};
+ //位运算
 var singleNumber = function(nums) {
    var num;
    for(var i=0;i<nums.length;i++){
