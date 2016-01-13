ac394acf558c69ab12d57ad0eb1d5901abb31f6c
marshallou <oucode@ccs.neu.edu>
2016-01-13 11:54:25 -0800

    add comment for 040

--- 040-unique-paths/Yiqi.js
+++ 040-unique-paths/Yiqi.js
@@ -3,6 +3,14 @@
  * @param {number} n
  * @return {number}
  */
+
+ //The problem has an obvious recursive pattern. The number of paths to grid[i][j]
+ //is equal to grid[i-1][j] + grid[i][j]. So we can easily solve it recursively.
+ //But in that way, we compute the same results repeatedly which makes me think that 
+ //we can use dynamic programming to reduce time complexity. We can define a two dimension
+ //array and compute from grid[0][0]. Thus we got time complexity o(n*m) solution.
+ //I notice the value of grid[i][j] is only based on grid[i-1][j] and grid[i][j-1], current value's
+ //upper and left element. Thus we can reduce space by using one dimensional array
 var uniquePaths = function(m, n) {
     var numOfPath = [];
     for(var i = 0; i < n; i++)
